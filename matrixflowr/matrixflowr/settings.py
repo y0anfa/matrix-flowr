d2628438eb49ae1957f8014a9eac6cfaec462665
yoanfa <yoanfaure@hotmail.fr>
2023-10-10 23:45:24 +0200

    fix vuln

--- matrixflowr/matrixflowr/settings.py
+++ matrixflowr/matrixflowr/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mwwzflw&kt&jk)!@hg+-^d-99ji7x#0kzc1p&hv&l66@kzwx&@'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
