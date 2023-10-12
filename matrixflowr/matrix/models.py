e1a883318ffd7c0b28554e2cba150f4e19e1dc0d
yoanfa <yoanfaure@hotmail.fr>
2023-10-12 14:39:55 +0200

    add default values to string fields

--- matrixflowr/matrix/models.py
+++ matrixflowr/matrix/models.py
@@ -9,11 +9,11 @@ class Matrix(models.Model):
         verbose_name = "matrix"
         verbose_name_plural = "matrices"
 
-    name = models.CharField(max_length=200, blank=True)
+    name = models.CharField(max_length=200, blank=True, default="")
     slug = models.SlugField(blank=True, null=True)
-    description = models.TextField(blank=True)
-    vendor = models.CharField(max_length=100, blank=True)
-    product = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True, default="")
+    vendor = models.CharField(max_length=100, blank=True, default="")
+    product = models.CharField(max_length=100, blank=True, default="")
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
@@ -25,12 +25,12 @@ class Flow(models.Model):
         db_table = "flows"
         verbose_name = "flow"
 
-    name = models.CharField(max_length=200, blank=True)
+    name = models.CharField(max_length=200, blank=True, default="")
     slug = models.SlugField(blank=True, null=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, default="")
     matrix = models.ForeignKey(Matrix, on_delete=models.CASCADE)
-    source = models.TextField(blank=True)
-    destination = models.TextField(blank=True)
+    source = models.TextField(blank=True, default="")
+    destination = models.TextField(blank=True, default="")
     is_opened = models.BooleanField(default=True, blank=True, null=False)
     is_active = models.BooleanField(default=False, blank=True, null=False)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
@@ -46,6 +46,6 @@ class Comment(models.Model):
 
     author = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     matrix = models.ForeignKey(Matrix, on_delete=models.CASCADE)
-    content = models.TextField(blank=True)
+    content = models.TextField(blank=True, default="")
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
